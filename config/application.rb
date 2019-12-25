3bbf36302c0fb613c893133587a0d956a20260b1
System Administrator <root@appentuss-MacBook-Pro-1.local>
2019-12-25 16:21:41 +0530

    wed

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module ReferenceAppi
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # -- all .rb fles in that directory are automatically loaded.
+    config.assets.initialize_on_precompile = false
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
